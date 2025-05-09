b66124f042cc4945676d48904f5a5a87ee1c5957
Lenício Jr <lenicio.junior@gmail.com>
2025-05-09 11:50:35 -0300

    adiciona tipagem e docstring à classe Livro

--- Livraria/livro.py
+++ Livraria/livro.py
@@ -1,5 +1,19 @@
+from typing import Optional, NoReturn
+
 class Livro:
-    def __init__(self, isbn, autor, editora, titulo, genero_literario):
+    def __init__(self, isbn: str, autor: str, editora: Optional[str, None],
+                 titulo: str, genero_literario: str) -> NoReturn:
+        """
+        Modelo de livro.
+
+        Args:
+            isbn: Código único de cada livro.
+            autor: Pessoa que escreveu o livro.
+            editora: Empresa que publicou. Este campo é opcional.
+            titulo: Nome do livro.
+            genero_literario: Genero ao qual o livro pertence. ex: Aventura, Romance, etc.
+        """
+
         self.isbn = isbn
         self.autor = autor
         self.editora = editora
@@ -7,11 +21,27 @@ class Livro:
         self.genero_literario = genero_literario
 
     @property
-    def isbn(self):
+    def isbn(self) -> str:
         return self.__isbn
 
     @isbn.setter
-    def isbn(self, isbn):
+    def isbn(self, isbn: str) -> None:
         if len(isbn) < 10 or len(isbn) > 13:
             raise ValueError('ISBN inválido. Deve conter 10 ou 13 dígitos.')
         self.__isbn = isbn
+
+
+# isbn = "123"  # ISBN inválido (muito curto)
+# autor = "J.R.R. Tolkien"
+# editora = "HarperCollins"
+# titulo = "O Senhor dos Anéis"
+# genero_literario = "Fantasia"
+#
+#
+# Livro(
+#     isbn=isbn,
+#     autor=autor,
+#     editora=editora,
+#     titulo=titulo,
+#     genero_literario=genero_literario
+# )
